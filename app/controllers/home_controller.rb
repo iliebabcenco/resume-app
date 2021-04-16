b43a0fdccaa810f079e5b021383a3e0c974fbd08
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-04-16 18:01:08 +0300

    Add download link to resume pdf

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def download_pdf
+    send_file "#{Rails.root}/public/Resume.pdf", type: "application/pdf", x_sendfile: true
+  end
 end
